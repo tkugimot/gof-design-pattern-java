36e7ef518ebdfa4c9d0bdf98fd4c2bdc217f5dd5
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2020-12-05 15:05:08 +0900

    Initial commit

---
+++ src/main/java/jp/tkugimot/gofdesignpatternjava/GofDesignPatternJavaApplication.java
@@ -0,0 +1,13 @@
+package jp.tkugimot.gofdesignpatternjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofDesignPatternJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofDesignPatternJavaApplication.class, args);
+	}
+
+}
